b1e621dfaf48ff56abb41216e5779e6c2426bb58
golden5611 <136337118+golden5611@users.noreply.github.com>
2023-11-13 11:41:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-document.querySelector('#som_tecla_pom').play();
-
-document.querySelector('.tecla_pom');
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick = tocarSomAplausos;
